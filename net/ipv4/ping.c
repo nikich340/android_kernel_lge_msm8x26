015c66aa91dbf0aaee4810dc4618997f27c440ba
Jane Zhou <a17711@motorola.com>
2014-11-24 11:44:08 -0800

    net/ping: handle protocol mismatching scenario
    ping_lookup() may return a wrong sock if sk_buff's and sock's protocols
    dont' match. For example, sk_buff's protocol is ETH_P_IPV6, but sock's
    sk_family is AF_INET, in that case, if sk->sk_bound_dev_if is zero, a wrong
    sock will be returned.
    the fix is to "continue" the searching, if no matching, return NULL.
    
    [cherry-pick of net 91a0b603469069cdcce4d572b7525ffc9fd352a6]
    
    Bug: 18512516
    Change-Id: I520223ce53c0d4e155c37d6b65a03489cc7fd494
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Alexey Kuznetsov <kuznet@ms2.inr.ac.ru>
    Cc: James Morris <jmorris@namei.org>
    Cc: Hideaki YOSHIFUJI <yoshfuji@linux-ipv6.org>
    Cc: Patrick McHardy <kaber@trash.net>
    Cc: netdev@vger.kernel.org
    Cc: stable@vger.kernel.org
    Signed-off-by: Jane Zhou <a17711@motorola.com>
    Signed-off-by: Yiwei Zhao <gbjc64@motorola.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    
    Conflicts:
    	net/ipv4/ping.c

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -214,8 +214,8 @@ static struct sock *ping_lookup(struct net *net, struct sk_buff *skb, u16 ident)
 					     &ipv6_hdr(skb)->daddr))
 				continue;
 #endif
-		} else { // handle protocol mismatching scenario, SR#01946325
-		    continue;
+		} else {
+			continue;
 		}
 
 		if (sk->sk_bound_dev_if && sk->sk_bound_dev_if != dif)
