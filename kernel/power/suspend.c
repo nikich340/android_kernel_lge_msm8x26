f4c95f00bc8c54589e243793de4e542e3c6a62f1
Ruchi Kandoi <kandoiruchi@google.com>
2015-05-07 10:18:55 -0700

    suspend: Return error when pending wakeup source is found.
    If a wakeup source is found to be pending in the last stage of suspend
    after syscore suspend then the device doesn't suspend but the error is
    not propogated which causes an error in the accounting for the number
    of suspend aborts and successful suspends.
    
    Change-Id: Ib63b4ead755127eaf03e3b303aab3c782ad02ed1
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -180,6 +180,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		if (!(suspend_test(TEST_CORE) || *wakeup)) {
 			error = suspend_ops->enter(state);
 			events_check_enabled = false;
+		} else if (*wakeup) {
+			error = -EBUSY;
 		}
 
 #if defined(CONFIG_LGE_WAKE_IRQ_PRINT)
