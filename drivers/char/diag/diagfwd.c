b15adb7f97d73dd0f7f6c3bfc0f60afba7cb64b3
Nikita <nikich340@gmail.com>
2015-08-26 12:09:22 +1000

    diag: Release wakeup sources properly

--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -602,7 +602,9 @@ int diag_process_smd_read_data(struct diag_smd_info *smd_info, void *buf,
 
 	return 0;
 err:
-	if (driver->logging_mode == MEMORY_DEVICE_MODE)
+	if ((smd_info->type == SMD_DATA_TYPE ||
+		 smd_info->type == SMD_CMD_TYPE) &&
+		driver->logging_mode == MEMORY_DEVICE_MODE)
 		diag_ws_on_read(0);
 
 #ifdef CONFIG_LGE_DM_APP
@@ -617,6 +619,12 @@ void diag_smd_queue_read(struct diag_smd_info *smd_info)
 {
 	if (!smd_info || !smd_info->ch)
 		return;
+
+	if ((smd_info->type == SMD_DATA_TYPE ||
+	     smd_info->type == SMD_CMD_TYPE) &&
+	     driver->logging_mode == MEMORY_DEVICE_MODE) {
+		diag_ws_on_notify();
+	}
 
 	switch (smd_info->type) {
 	case SMD_DCI_TYPE:
@@ -871,9 +879,11 @@ void diag_smd_send_req(struct diag_smd_info *smd_info)
 				}
 			}
 		}
-		if (smd_info->type == SMD_DATA_TYPE &&
-		    driver->logging_mode == MEMORY_DEVICE_MODE)
-			diag_ws_on_read(pkt_len);
+
+		if ((smd_info->type == SMD_DATA_TYPE ||
+		     smd_info->type == SMD_CMD_TYPE) &&
+		     driver->logging_mode == MEMORY_DEVICE_MODE)
+			diag_ws_on_read(total_recd);
 
 #ifdef CONFIG_LGE_DM_APP
         if (smd_info->type == SMD_DATA_TYPE &&
@@ -2516,11 +2526,11 @@ void diag_smd_notify(void *ctxt, unsigned event)
 			diag_dci_notify_client(smd_info->peripheral_mask,
 							DIAG_STATUS_OPEN);
 		}
-		wake_up(&driver->smd_wait_q);
 		diag_smd_queue_read(smd_info);
+		wake_up(&driver->smd_wait_q);
 	} else if (event == SMD_EVENT_DATA) {
-		wake_up(&driver->smd_wait_q);
 		diag_smd_queue_read(smd_info);
+		wake_up(&driver->smd_wait_q);
 		if (smd_info->type == SMD_DCI_TYPE ||
 		    smd_info->type == SMD_DCI_CMD_TYPE) {
 			diag_dci_try_activate_wakeup_source();
